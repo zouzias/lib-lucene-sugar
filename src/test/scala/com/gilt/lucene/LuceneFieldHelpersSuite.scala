08834adfa433038c0a60ce05067204e69d561d1d
Federico Feroldi <fferoldi@gilt.com>
2013-07-18 10:08:17 -0400

    tests for Float and Double helpers
    Change-Id: I9e7c8cdb7fff83cfd663b23334822030208e990e

--- src/test/scala/com/gilt/lucene/LuceneFieldHelpersSuite.scala
+++ src/test/scala/com/gilt/lucene/LuceneFieldHelpersSuite.scala
@@ -3,19 +3,20 @@ package com.gilt.lucene
 import org.scalatest.FlatSpec
 import org.scalatest.matchers.ShouldMatchers
 import org.scalatest.mock.MockitoSugar
-import org.apache.lucene.document.Document
+import org.apache.lucene.document.{Field, Document}
 import LuceneFieldHelpers._
 import LuceneText._
+import org.apache.lucene.index.IndexableField
 
 class LuceneFieldHelpersSuite extends FlatSpec with ShouldMatchers with MockitoSugar {
 
-  def searchableReadableField[T: LuceneFieldLike](value: T) {
+  def searchableReadableField[T: LuceneFieldLike](value: T, valueReader: IndexableField => T) {
 
     it should "add an indexed only field" in {
       val doc = new Document
       doc.addIndexedOnlyField("field", value)
       val field = doc.getField("field")
-      field.stringValue() should equal(value.toString)
+      valueReader(field) should equal(value)
       field.fieldType() should be ('indexed)
       field.fieldType() should not be ('stored)
     }
@@ -24,7 +25,7 @@ class LuceneFieldHelpersSuite extends FlatSpec with ShouldMatchers with MockitoS
       val doc = new Document
       doc.addStoredOnlyField("field", value)
       val field = doc.getField("field")
-      field.stringValue() should equal(value.toString)
+      valueReader(field) should equal(value)
       field.fieldType() should not be ('indexed)
       field.fieldType() should be ('stored)
     }
@@ -33,19 +34,23 @@ class LuceneFieldHelpersSuite extends FlatSpec with ShouldMatchers with MockitoS
       val doc = new Document
       doc.addIndexedStoredField("field", value)
       val field = doc.getField("field")
-      field.stringValue() should equal(value.toString)
+      valueReader(field) should equal(value)
       field.fieldType() should be ('indexed)
       field.fieldType() should be ('stored)
     }
 
   }
 
-  "String helper" should behave like searchableReadableField("string")
+  "String helper" should behave like searchableReadableField("string", _.stringValue())
 
-  "Int helper" should behave like searchableReadableField(10)
+  "Text helper" should behave like searchableReadableField("text".toLuceneText, { f => LuceneText(f.stringValue()) })
 
-  "Long helper" should behave like searchableReadableField(1000000L)
+  "Int helper" should behave like searchableReadableField(10, _.numericValue().intValue())
 
-  "Text helper" should behave like searchableReadableField("text".toLuceneText)
+  "Long helper" should behave like searchableReadableField(1000000L, _.numericValue().longValue())
+
+  "Float helper" should behave like searchableReadableField(12.1f, _.numericValue().floatValue())
+
+  "Double helper" should behave like searchableReadableField(12.1d, _.numericValue().doubleValue())
 
 }
